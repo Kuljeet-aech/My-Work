48512d7b9b748faff4aead153300bbf4af392d06
Kuljeet-aech <kuljeet.aech@gmail.com>
2021-07-19 22:04:45 +0530

    .

--- FirstCode.R
+++ FirstCode.R
@@ -1,8 +1,4 @@
 library(tidyverse)
 library(dslabs)
 data(murders)
-<<<<<<< HEAD
 murders %>% ggplot(aes(population, total, label=abb, color=region)) + geom_label()
-=======
-murders %>% ggplot(aes(population, total, label=abb, color=region)) + geom_label()
->>>>>>> 52579ed77532bffaec69e7d99c92c2a780e8e8b9
